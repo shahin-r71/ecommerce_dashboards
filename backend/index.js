aeb3a8d5532bdeb535d182c4f52a11825e84640e
shahin-r71 <shahinlv01@gmail.com>
2025-06-29 00:04:44 +0600

    initialize project

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Backend API Server!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
