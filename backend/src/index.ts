44866f925699ba1cbc88324772fb02afa57cc2a0
shahin-r71 <shahinlv01@gmail.com>
2025-06-29 16:15:24 +0600

    add typescript configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
-const express = require('express');
+import express from 'express'; 
+import dotenv from 'dotenv';
+
 const app = express();
+
+dotenv.config();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
